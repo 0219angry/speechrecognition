d32210c12ebb452839c29c6c827981ba86558eb2
0219Angry <angry.yuma.0219@gmail.com>
2024-08-11 19:47:32 +0900

    add download.py 青空朗読の番号を指定してmp3を取得

---
+++ download.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://aozoraroudoku.jp/voice/'
+
+print("Enter 青空朗読 contents number (#### in aozoraroudoku.jp/voice/rdp/rd####.html)")
+number = input()
+
+url_data = requests.get(url+'mp3/rd'+number+'.mp3').content
+
+with open('data/mp3/rd'+number+'.mp3',mode='wb') as f:
+  f.write(url_data)
+  
+html_data = requests.get(url+'rdp/rd'+number+'.html')
+html_data.encoding = html_data.apparent_encoding
+soup = BeautifulSoup(html_data.text, 'html.parser')
+
+title = soup.title.string
+print("Download file from \""+title+"\"")
