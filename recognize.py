0b823236423c5138b905bc914a62428517a4390f
0219Angry <angry.yuma.0219@gmail.com>
2024-08-13 00:19:11 +0900

    fix recognize.py エラー発生時に再試行

--- recognize.py
+++ recognize.py
@@ -3,18 +3,29 @@ import pyaudio
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 import datetime
+import time
 
 dir = "data/wav/"
 
-def recognize(number):
+def recognize(number, max_retries=5):
   sprec = sr.Recognizer()
+  retries = 0
+  while retries < max_retries:
+    try:
+      print(f"[{datetime.datetime.now()}] {dir}rd{number}.wav ")
+      with sr.AudioFile(dir+"rd"+number+".wav") as source:
+        audio = sprec.record(source)
+      script = sprec.recognize_google(audio, language='ja-JP')
 
-  print(f"[{datetime.datetime.now()}] {dir}rd{number} ========\n")
-  with sr.AudioFile(dir+"rd"+number+".wav") as source:
-    audio = sprec.record(source)
-  script = sprec.recognize_google(audio, language='ja-JP')
-
-  with open("data/txt/"+"rd"+number+".txt",'w') as f:
-    f.write(script)
-  print(script)
+      with open("data/txt/"+"rd"+number+".txt",'w') as f:
+        f.write(script)
+      print(script)
+      break
+    except sr.RequestError as e:
+      print(f"Recognition failed: {e}. Retrying {retries + 1}/{max_retries}...")
+      retries += 1
+      time.sleep(1)
+  
+  if retries == max_retries:
+    print(f"Failed to recognize after {max_retries} attempts.")
 
