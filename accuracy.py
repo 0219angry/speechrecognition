7c3f868724adf1d7828c7381dbfffde9d27c2d14
0219Angry <angry.yuma.0219@gmail.com>
2024-08-12 16:13:26 +0900

    add calc_accuracy() 認識精度を計算する

--- accuracy.py
+++ accuracy.py
@@ -4,6 +4,101 @@ from bs4 import BeautifulSoup
 # 無視する文字の一覧
 ignore_letters =  ["。","、","\n","\t"," ","　","○","「","」","（","）","-","―","！","!","？","?"]
 
+
+
+def calc_accuracy(ans, rec):
+  N = len(ans) # 正解文の長さ
+  delection_error = 0 # 削除誤り
+  substitution_error = 0 # 置換誤り
+  insertion_error = 0 # 挿入誤り
+  
+  c_ans = 0 # 正解文内の現在地
+  c_rec = 0 # 認識文内の現在地
+  error_encount = False # true: 現在エラーの処理中
+  error_encount_ans = None # 直近の誤り発生地点(正解文)
+  error_encount_rec = None # 直近の誤り発生地点(認識文)
+  
+  while c_ans < N:
+    if error_encount == True:
+      # 正解文を進める(削除誤り)
+      while c_ans < len(ans) and ans[c_ans] != rec[c_rec]:
+        print(f"<D>[{ans[c_ans]}] <=> [{rec[c_rec]}] {c_ans}|{c_rec}")
+        c_ans += 1
+        
+      d = c_ans - error_encount_ans
+      c_ans = error_encount_ans
+      
+      # 認識文を進める(挿入誤り)
+      while c_rec < len(rec) and ans[c_ans] != rec[c_rec]:
+        print(f"<I>[{ans[c_ans]}] <=> [{rec[c_rec]}] {c_ans}|{c_rec}")
+        c_rec += 1
+        
+      i = c_rec - error_encount_rec
+      c_rec = error_encount_rec
+      
+      # どちらも進める(置換誤り)
+      while c_rec < len(rec) and c_ans < len(ans) and ans[c_ans] != rec[c_rec]:
+        print(f"<S>[{ans[c_ans]}] <=> [{rec[c_rec]}] {c_ans}|{c_rec}")
+        c_rec += 1
+        c_ans += 1
+      s = c_rec - error_encount_rec
+      c_rec = error_encount_rec
+      c_ans = error_encount_ans
+      
+      # 値が小さいほうを採用する
+      if i <= d and i <= s: # 挿入誤り 
+        insertion_error += i
+        c_rec = c_rec + i
+        error_encount = False
+        print(f"insertion error detected: {i} letter(s).")
+      elif d <= i and d <= s: # 削除誤り
+        delection_error += d
+        c_ans = c_ans + d
+        error_encount = False
+        print(f"delection error detected: {d} letter(s).")
+      else: # 置換誤り
+        substitution_error += s
+        c_ans = c_ans + s
+        c_rec = c_rec + s
+        error_encount = False
+        print(f"substitution error detected: {s} letter(s).")
+    
+    # 正解文の終端に来たのに認識文が残っているときは挿入誤り    
+    if c_ans == len(ans):
+      i = len(rec)-c_rec-1
+      insertion_error += i
+      print(f"insertion error detected: {i} letter(s).")
+      break
+    
+    # 認識文の終端に来たのに正解文が残っているときは削除誤り
+    if c_rec == len(rec):
+      d = len(ans)-c_ans-1
+      delection_error += d
+      print(f"delection error detected: {d} letter(s).")
+      break
+      
+        
+    if ans[c_ans] != rec[c_rec] and error_encount == False:
+      print(f"<E>[{ans[c_ans]}] <=> [{rec[c_rec]}] {c_ans}|{c_rec}")
+      error_encount = True
+      error_encount_ans = c_ans
+      error_encount_rec = c_rec
+      continue
+    
+    if ans[c_ans] == rec[c_rec]:
+      print(f"<C>[{ans[c_ans]}] <=> [{rec[c_rec]}] {c_ans}|{c_rec}")
+      c_ans += 1
+      c_rec += 1
+  
+  print(f"全文字数: {N}")
+  print(f"削除誤り: {delection_error}")
+  print(f"置換誤り: {substitution_error}")    
+  print(f"挿入誤り: {insertion_error}")
+  print(f"accuracy: {(N-delection_error-substitution_error-insertion_error)/N} %")
+  return (N-delection_error-substitution_error-insertion_error)/N
+  
+
+
 def accuracy():
   print("Enter 青空朗読 contents number (#### in aozoraroudoku.jp/voice/rdp/rd####.html)")
   number = input()
@@ -23,20 +118,27 @@ def accuracy():
   with open("data/txt/"+"rd"+number+".txt",'r') as f:
     recognized_text = f.read()
     
-  with open("debug.txt",'w') as f:
-    f.write(answer_text)
+  formatted_answer = []
+  
   for l in answer_text:
     if l not in ignore_letters:
       print(l,end="")
-    # else:
-    #   print(f"[{l}]", end="")
+      formatted_answer.append(l)
+      
   print("\n")
   
+  formatted_recog_text = []
   
   for l in recognized_text:
     if l not in ignore_letters:
       print(l,end="")
+      formatted_recog_text.append(l)
   print("\n")
   
+  calc_accuracy(formatted_answer, formatted_recog_text)
+  
+  
+
+print(calc_accuracy("本日もご利用いただきありがとうございます","本日もご利用ありがとうございました"))
     
-accuracy()
+# accuracy()
